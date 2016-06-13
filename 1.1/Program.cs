3880d3da258bb607efb7ff35fd3bd52c944f4a5a
alexkoha <alexkoha@gmail.com>
2016-06-13 16:12:10 +0300

    Update Program.cs

--- 1.1/Program.cs
+++ 1.1/Program.cs
@@ -11,7 +11,7 @@ namespace Lab1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello ");
-            Console.WriteLine("      ");
+            Console.WriteLine("Alex Kohatzki , Arava , Karmiel.");
 
         }
         
